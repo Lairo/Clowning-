2dc8e76972abaae84346b831ebd524bb5ce5d75d
chajus1 <chajus1@googlemail.com>
2024-09-07 06:45:02 +0100

    Add project files.

---
+++ ConsoleApp2/Clown.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Clown 
+    {
+        public string? Name;
+        public int Height;
+
+        public void WhoAreYou()
+        {
+            Console.WriteLine($"Hi my name is {Name} and I am {Height} inches tall");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Clown oneClown = new Clown();
+            oneClown.Name = "Boffo";
+            oneClown.Height = 14;
+            oneClown.WhoAreYou();
+
+            Clown anotherClown = new Clown();
+            anotherClown.Name = "Biff";
+            anotherClown.Height = 16;
+            anotherClown.WhoAreYou();
+
+            Clown clown3 = new Clown();
+            clown3.Name = anotherClown.Name;
+            clown3.Height = oneClown.Height - 3;
+            clown3.WhoAreYou();
+
+            anotherClown.Height *= 2;
+            anotherClown.WhoAreYou();
+        }
+    }
+}
